f3307d90468e8881c67bddca973ab9a4cfbfbfd3
Kris13561 <folseorno@gmail.com>
2024-10-26 17:03:03 +0300

    add documentation for votes and polls contexts

--- lib/polls_app/polls.ex
+++ lib/polls_app/polls.ex
@@ -2,10 +2,26 @@ defmodule PollsApp.Polls do
   import Ecto.Query
   alias PollsApp.{Repo, Poll, Vote}
 
+  @doc """
+  Creates a changeset for a new poll using the given attributes.
+
+  ## Example
+      iex> attrs = %{"name" => "Best Programming Language"}
+      iex> PollsApp.Polls.poll_changeset(attrs)
+      %Ecto.Changeset<action: nil, changes: %{name: "Best Programming Language"}, ...>
+  """
   def poll_changeset(attrs \\ %{}) do
     Poll.changeset(%Poll{}, attrs)
   end
 
+  @doc """
+  Creates a new poll in the database.
+
+  ## Example
+      iex> attrs = %{"name" => "Best Food","user_id" => 1, "options" => ["One", "Two"]}
+      iex> PollsApp.Polls.create_poll(attrs)
+      {:ok, %Poll{name: "Best Food", ...}}
+  """
   def create_poll(attrs) do
     existing_poll = Repo.get_by(Poll, name: attrs["name"])
 
@@ -19,21 +35,51 @@ defmodule PollsApp.Polls do
     end
   end
 
+  @doc """
+  Retrieves a poll by its ID and preloads associated votes.
+
+  ## Example
+      iex> PollsApp.Polls.get_poll!(1)
+      %Poll{id: 1, name: "Best Programming Language", options: [...], ...}
+  """
   def get_poll!(id) do
     Repo.get!(Poll, id)
     |> Repo.preload(:votes)
   end
 
+  @doc """
+  Updates an existing poll with new attributes.
+
+  ## Example
+      iex> poll = PollsApp.Polls.get_poll!(1)
+      iex> PollsApp.Polls.update_poll(poll, %{"name" => "Best Food"})
+      {:ok, %Poll{id: 1, name: "Best Food"}, ...}
+  """
   def update_poll(%Poll{} = poll, attrs) do
     poll
     |> Poll.changeset(attrs)
     |> Repo.update()
   end
 
+  @doc """
+  Deletes a poll from the database.
+
+  ## Example
+      iex> poll = PollsApp.Polls.get_poll!(1)
+      iex> PollsApp.Polls.delete_poll(poll)
+      {:ok, %PollsApp.Poll{__meta__: #Ecto.Schema.Metadata<:deleted, "polls">, id: 1, ...}
+  """
   def delete_poll(%Poll{} = poll) do
     Repo.delete(poll)
   end
 
+  @doc """
+  Lists all polls along with their authors.
+
+  ## Example
+      iex> PollsApp.Polls.list_all_polls_with_authors()
+      [{1, "Best Programming Language", "author1"}, {2, "Best Food", "author2"}]
+  """
   def list_all_polls_with_authors do
     Poll
     |> Repo.all()
@@ -43,12 +89,26 @@ defmodule PollsApp.Polls do
     end)
   end
 
+  @doc """
+  Retrieves all votes associated with a specific poll by its ID.
+
+  ## Example
+      iex> PollsApp.Polls.get_votes_by_poll_id(1)
+      [%PollsApp.Vote{...}, %PollsApp.Vote{...}]
+  """
   def get_votes_by_poll_id(poll_id) do
     Vote
     |> where([v], v.poll_id == ^poll_id)
     |> Repo.all()
   end
 
+  @doc """
+  Calculates the distribution of votes for a specific poll.
+
+  ## Example
+      iex> PollsApp.Polls.votes_distribution(1)
+      [{"Option A", 10, 50.0}, {"Option B", 10, 50.0}]
+  """
   def votes_distribution(poll_id) do
     votes = get_votes_by_poll_id(poll_id)
     options = get_poll_options(poll_id)
@@ -62,11 +122,25 @@ defmodule PollsApp.Polls do
     end)
   end
 
+  @doc """
+  Retrieves the options available for a specific poll.
+
+  ## Example
+      iex> PollsApp.Polls.get_poll_options(1)
+      ["Option A", "Option B"]
+  """
   def get_poll_options(poll_id) do
     poll = Repo.get!(Poll, poll_id)
     poll.options || []
   end
 
+  @doc """
+  Gets the distribution of votes for all polls along with their authors.
+
+  ## Example
+      iex> PollsApp.Polls.polls_votes_distribution()
+      %{1 => [{"Option A", 10, 50.0}, {"Option B", 10, 50.0}]}
+  """
   def polls_votes_distribution do
     list_all_polls_with_authors()
     |> Enum.map(fn {poll_id, _, _} ->
@@ -75,6 +149,13 @@ defmodule PollsApp.Polls do
     |> Enum.into(%{})
   end
 
+  @doc """
+  Simple way to seeds a new poll with the given attributes.
+
+  ## Example
+      iex> PollsApp.Polls.seeds_poll(%{"name" => "Best Food","user_id" => 1, "options" => ["One", "Two"]})
+      {:ok, %Poll{name: "Best Food", ...}}
+  """
   def seeds_poll(attrs) do
     %Poll{}
     |> Poll.changeset(attrs)
--- lib/polls_app/votes.ex
+++ lib/polls_app/votes.ex
@@ -1,16 +1,40 @@
 defmodule PollsApp.Votes do
   alias PollsApp.{Repo, Vote, Accounts.User}
 
+  @doc """
+  Creates a new vote.
+
+  ## Example
+      iex> attrs = %{"user_id" => 1, "poll_id" => 2, "option" => "Option A"}
+      iex> PollsApp.Votes.create_vote(attrs)
+      {:ok,  %PollsApp.Vote{__meta__: #Ecto.Schema.Metadata<:loaded, "votes">, id: 17, user_id: 1, ...}
+  """
   def create_vote(attrs) do
     %Vote{}
     |> Vote.changeset(attrs)
     |> Repo.insert()
   end
 
+  @doc """
+  Deletes a vote.
+
+  ## Example
+      iex> attrs = %{"user_id" => 3, "poll_id" => 2, "option" => "Option A"}
+      iex> {:ok, vote} = PollsApp.Votes.create_vote(attrs)
+      iex> PollsApp.Votes.delete_vote(vote)
+      {:ok, %PollsApp.Vote{__meta__: #Ecto.Schema.Metadata<:deleted, "votes">, user_id: 1, poll_id: 2, ...}}
+  """
   def delete_vote(%Vote{} = vote) do
     Repo.delete(vote)
   end
 
+  @doc """
+  Retrieves the vote cast by a user for a specific poll, if any.
+
+  ## Example
+      iex> PollsApp.Votes.get_user_vote_for_poll(1, 2)
+      "You voted for: Option A"
+  """
   def get_user_vote_for_poll(user_id, poll_id) do
     user = Repo.get!(User, user_id)
     |> Repo.preload(:votes)
@@ -22,6 +46,13 @@ defmodule PollsApp.Votes do
     end
   end
 
+  @doc """
+  Deletes a user's vote for a specific poll.
+
+  ## Example
+      iex> PollsApp.Votes.delete_vote(1, 2)
+      {:ok,  %PollsApp.Vote{__meta__: #Ecto.Schema.Metadata<:deleted, "votes">,id: 1, user_id:1, ...}
+  """
   def delete_vote(user_id, poll_id) do
     vote = Repo.get_by(Vote, user_id: user_id, poll_id: poll_id)
 
